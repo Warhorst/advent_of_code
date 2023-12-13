483a170dd72fa04dc471c1e3802d083c689ca9eb
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-12-13 17:56:29 +0100

    Added existing solutions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
+use std::fmt::Debug;
+use std::fs::read_to_string;
+use crate::y2023::d1::{solve_1a, solve_1b};
+use crate::y2023::d10::{solve_10a, solve_10b};
+use crate::y2023::d11::{solve_11a, solve_11b};
+use crate::y2023::d12::{solve_12a, solve_12b};
+use crate::y2023::d13::{solve_13a, solve_13b};
+use crate::y2023::d2::{solve_2a, solve_2b};
+use crate::y2023::d3::{solve_3a, solve_3b};
+use crate::y2023::d4::{solve_4a, solve_4b};
+use crate::y2023::d5::{solve_5a, solve_5b};
+use crate::y2023::d6::{solve_6a, solve_6b};
+use crate::y2023::d7::{solve_7a, solve_7b};
+use crate::y2023::d8::{solve_8a, solve_8b};
+use crate::y2023::d9::{solve_9a, solve_9b};
+
+mod y2023;
+
 fn main() {
-    println!("Hello, world!");
+    solve_day(1)
+}
+
+fn solve_day(day: usize) {
+    let solve_day_funcs = [
+        || solve(1, 2023, solve_1a, 142, solve_1b, 281),
+        || solve(2, 2023, solve_2a, 8, solve_2b, 2286),
+        || solve(3, 2023, solve_3a, 4361, solve_3b, 467835),
+        || solve(4, 2023, solve_4a, 13, solve_4b, 30),
+        || solve(5, 2023, solve_5a, 35, solve_5b, 46),
+        || solve(6, 2023, solve_6a, 288, solve_6b, 71503),
+        || solve(7, 2023, solve_7a, 6440, solve_7b, 5905),
+        || solve(8, 2023, solve_8a, 6, solve_8b, 6),
+        || solve(9, 2023, solve_9a, 114, solve_9b, 2),
+        || solve(10, 2023, solve_10a, 8, solve_10b, 10),
+        || solve(11, 2023, solve_11a, 374, solve_11b, 82000210),
+        || solve(12, 2023, solve_12a, 21, solve_12b, 525152),
+        || solve(13, 2023, solve_13a, 405, solve_13b, 0),
+    ];
+
+    solve_day_funcs[day - 1]()
+}
+
+fn solve<A: Debug + PartialEq, B: Debug + PartialEq, AS: Fn(&str) -> A, BS: Fn(&str) -> B>(
+    day: u8,
+    year: u16,
+    a_solver: AS,
+    a_example_solution: A,
+    b_solver: BS,
+    b_example_solution: B,
+) {
+    println!("Solving day {day}, {year}");
+
+    let input = load_input(day, year);
+
+    if input.example_a.is_empty() {
+        println!("example a does not exist yet, skipping it");
+    } else {
+        assert_eq!(a_example_solution, a_solver(&input.example_a));
+        println!("Example a works");
+    }
+
+    if input.example_b.is_empty() {
+        println!("example b does not exist yet, skipping it");
+    } else {
+        assert_eq!(b_example_solution, b_solver(&input.example_b));
+        println!("Example b works");
+    }
+
+    if input.puzzle_input.is_empty() {
+        println!("the puzzle input does not exist yet, skipping it");
+    } else {
+        println!("Solution a: {:?}", a_solver(&input.puzzle_input));
+        println!("Solution b: {:?}", b_solver(&input.puzzle_input));
+    }
+}
+
+pub struct Input {
+    pub puzzle_input: String,
+    pub example_a: String,
+    pub example_b: String
+}
+
+pub fn load_input(day: u8, year: u16) -> Input {
+    let puzzle_input = read_to_string(format!("./input/{year}/{day}/p.txt")).unwrap_or(String::default());
+    let example_a = read_to_string(format!("./input/{year}/{day}/ea.txt")).unwrap_or(String::default());
+    let example_b = read_to_string(format!("./input/{year}/{day}/eb.txt")).unwrap_or(String::default());
+
+    Input {
+        puzzle_input,
+        example_a,
+        example_b
+    }
 }
---
+++ src/y2023/d12.rs
@@ -0,0 +1,141 @@
+use rayon::iter::ParallelIterator;
+use rayon::prelude::IntoParallelIterator;
+
+use Spring::*;
+
+pub fn solve_12a(input: &str) -> usize {
+    let rows = input
+        .lines()
+        .map(Row::from)
+        .collect::<Vec<_>>();
+
+    rows
+        .into_par_iter()
+        .map(|r| r.get_count_resolved_rows())
+        .sum()
+}
+
+// Idea: Windows iter
+// start: all windows are as far left as possible
+// -> Return this start as first result
+// the iterator is done if all windows are as far right as possible
+// 1. Move the current to the right until valid
+// 2. If on valid position, go to the next window
+// 3. if the last window reached a valid position, store the config
+// 4. continue with the current position until a non valid position was reached
+// 5. backtrack to the point before moving the current position, move the previous one
+// 6. Repeat until done
+pub fn solve_12b(_input: &str) -> usize {
+    0
+}
+
+#[derive(Clone, Debug)]
+struct Row {
+    springs: Vec<Spring>,
+    nums: Vec<usize>,
+}
+
+impl Row {
+    fn get_count_resolved_rows(&self) -> usize {
+        let num_unknown_springs = self.springs.iter().filter(|s| **s == Unknown).count();
+
+        Self::create_bit_representations(num_unknown_springs)
+            .into_iter()
+            .map(Self::bits_to_springs)
+            .map(|springs| self.create_resolved_row(springs))
+            .filter(|row| row.is_valid())
+            .count()
+    }
+
+    fn create_bit_representations(num_unknown_springs: usize) -> impl IntoIterator<Item=Vec<u32>> {
+        (0..2_u32.pow(num_unknown_springs as u32))
+            .into_iter()
+            .map(move |x| (0..num_unknown_springs)
+                .into_iter()
+                .map(|n| (x >> n) & 1)
+                .collect::<Vec<_>>())
+    }
+
+    fn bits_to_springs(bits: Vec<u32>) -> Vec<Spring> {
+        bits
+            .into_iter()
+            .map(|bit| if bit == 0 {
+                Damaged
+            } else {
+                Operational
+            })
+            .collect()
+    }
+
+    fn create_resolved_row(&self, resolves: Vec<Spring>) -> Row {
+        let mut new = self.clone();
+        let mut iter = resolves.into_iter();
+        new.springs.iter_mut().for_each(|s| if *s == Unknown {
+            *s = iter.next().unwrap()
+        });
+
+        new
+    }
+
+    fn is_valid(&self) -> bool {
+        let mut on_block = false;
+        let mut current_block_count = 0;
+        let mut block_sizes = vec![];
+
+        for i in 0..self.springs.len() {
+            match self.springs[i] {
+                Operational => {
+                    if on_block {
+                        block_sizes.push(current_block_count);
+                        current_block_count = 0;
+                    }
+
+                    on_block = false
+                }
+                Damaged => {
+                    on_block = true;
+                    current_block_count += 1;
+                }
+                Unknown => panic!("should not be unknown at this point")
+            }
+        }
+
+        if on_block {
+            block_sizes.push(current_block_count);
+        }
+
+        block_sizes == self.nums
+    }
+}
+
+impl From<&str> for Row {
+    fn from(s: &str) -> Self {
+        let split = s.split(" ").collect::<Vec<_>>();
+
+        let springs = split[0].chars().map(Spring::from).collect();
+        let nums = split[1].split(",").map(|s| s.parse::<usize>().unwrap()).collect();
+
+        Row {
+            springs,
+            nums,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+enum Spring {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
+impl From<char> for Spring {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Operational,
+            '#' => Damaged,
+            '?' => Unknown,
+            _ => panic!("unknown char")
+        }
+    }
+}
