4618cf0cae8b685e6e83545dd56b9e464886ccb2
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-12-15 16:02:40 +0100

    Refactored 2024 15

--- src/y2024/d15.rs
+++ src/y2024/d15.rs
@@ -80,6 +80,7 @@ pub fn solve_b(input: &str) -> usize {
     use TileB::*;
 
     let mut split = input.split("\n\n");
+    // transform the input to its double-sized version
     let expanded_input = split
         .next()
         .unwrap()
@@ -107,15 +108,15 @@ pub fn solve_b(input: &str) -> usize {
         .lines()
         .flat_map(|line| line.chars().map(map_to_dir))
         .for_each(|dir| {
-            match dir {
-                XP | XM => {
-                    let pos_in_dir = bot_pos.position_in_direction(dir, 1);
+            let pos_in_dir = bot_pos.position_in_direction(dir, 1);
 
-                    match board.get_tile(pos_in_dir).copied().unwrap() {
-                        // a box is the neighbour, check if it can be pushed
-                        BoxL | BoxR => {
+            match board.get_tile(pos_in_dir).copied().unwrap() {
+                // a box is the neighbour, check if it can be pushed
+                BoxL | BoxR => {
+                    match dir {
+                        XP | XM => {
+                            // the same logic applies as in part A if the bot moves left or right
                             let mut step = 2;
-
                             loop {
                                 let farther_away = bot_pos.position_in_direction(dir, step);
 
@@ -146,59 +147,36 @@ pub fn solve_b(input: &str) -> usize {
                                 }
                             }
                         }
-                        // next to a wall, don't move
-                        Wall => {}
-                        // free, just move there and free the current position
-                        Free => {
+                        // if the bot moves up or down, check recursively if the adjacent boxes are all movable
+                        YP | YM => if adjacent_boxes_movable(pos_in_dir, dir, &board) {
+                            // combine the box positions with their tile type (left or right)
+                            let boards_and_tiles = collect_adjacent_boxes(pos_in_dir, dir, &board)
+                                .into_iter()
+                                .map(|pos| (pos, board.get_tile(*pos).copied().unwrap()))
+                                .collect::<Vec<_>>();
+
+                            // clear all the current box positions by setting them to Free
+                            boards_and_tiles.iter().for_each(|(pos, _)| board.set_tile(**pos, Free));
+                            // put all the box tiles one up or down, depending on the direction
+                            boards_and_tiles.iter().for_each(|(pos, tile)| board.set_tile(pos.position_in_direction(dir, 1), *tile));
+
+                            // finally, also move the bot in the direction and update its current position
                             board.set_tile(bot_pos, Free);
                             board.set_tile(pos_in_dir, Bot);
                             bot_pos = pos_in_dir;
-                        }
-                        // only one bot exists
+                        },
                         _ => unreachable!()
                     }
                 }
-                YP | YM => {
-                    let pos_in_dir = bot_pos.position_in_direction(dir, 1);
-
-                    match board.get_tile(pos_in_dir).copied().unwrap() {
-                        // a box is the neighbour, check if it can be pushed
-                        BoxL | BoxR => {
-                            if adjacent_boxes_movable(pos_in_dir, dir, &board) {
-                                let boxes = collect_adjacent_boxes(pos_in_dir, dir, &board);
-                                //boxes.sort_by(|a, b| match dir {
-                                //    YM => a.y.cmp(&b.y),
-                                //    YP => b.y.cmp(&a.y),
-                                //    _ => unreachable!()
-                                //});
-
-                                //println!("{:?}", boxes);
-
-                                let boards_and_tiles = boxes
-                                    .iter()
-                                    .map(|pos| (pos, board.get_tile(*pos).copied().unwrap()))
-                                    .collect::<Vec<_>>();
-
-                                boards_and_tiles.iter().for_each(|(pos, _)| board.set_tile(**pos, Free));
-                                boards_and_tiles.iter().for_each(|(pos, tile)| board.set_tile(pos.position_in_direction(dir, 1), *tile));
-
-                                board.set_tile(bot_pos, Free);
-                                board.set_tile(pos_in_dir, Bot);
-                                bot_pos = pos_in_dir;
-                            }
-                        }
-                        // next to a wall, don't move
-                        Wall => {}
-                        // free, just move there and free the current position
-                        Free => {
-                            board.set_tile(bot_pos, Free);
-                            board.set_tile(pos_in_dir, Bot);
-                            bot_pos = pos_in_dir;
-                        }
-                        // only one bot exists
-                        _ => unreachable!()
-                    }
+                // next to a wall, don't move
+                Wall => {}
+                // free, just move there and free the current position
+                Free => {
+                    board.set_tile(bot_pos, Free);
+                    board.set_tile(pos_in_dir, Bot);
+                    bot_pos = pos_in_dir;
                 }
+                // only one bot exists
                 _ => unreachable!()
             }
         });
