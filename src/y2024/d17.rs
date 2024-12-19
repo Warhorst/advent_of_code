016086e87094b372398e3081432b53a4c03a36f6
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-12-19 19:03:00 +0100

    Solved 2024 17 B

--- src/y2024/d17.rs
+++ src/y2024/d17.rs
@@ -1,57 +1,20 @@
-use std::iter::Rev;
 use crate::aoc_lib::*;
 use itertools::Itertools;
 use Ins::*;
-use rayon::prelude::*;
 
 pub fn solve_a(input: &str) -> String {
-    let mut device = Device::new(input);
-
-    //println!("{:?}", device);
-
-    device.run()
+    Device::new(input)
+        .run()
+        .into_iter()
+        .map(|val| val.to_string())
+        .join(",")
 }
 
 pub fn solve_b(input: &str) -> usize {
     let device = Device::new(input);
-    let searched = device.original_instructions.iter().map(|i| i.to_string()).join(",");
+    let foo = device.run_b();
 
-    0
-
-    //(1..8)
-    //    .into_iter()
-    //    .map(|i| {
-    //        let mut clone = device.clone();
-    //        let a =  i * 8usize.pow(device.original_instructions.len() as u32 - 1);
-    //        clone.reg_a = a;
-    //        let result = clone.run();
-    //        (a, result)
-    //    })
-    //    .inspect(|(a, result)| println!("{a} - {result}"))
-    //    .find_map(|(a, result)| {
-    //        if result == device.original_instructions.iter().map(|i| i.to_string()).join(",") {
-    //            Some(a)
-    //        } else {
-    //            None
-    //        }
-    //    })
-    //    .unwrap()
-
-    //// bad brute force method
-    //let min = 8usize.pow(device.original_instructions.len() as u32 - 1);
-    //let max = 7 * min;
-//
-    //let foo = (min..max)
-    //    .par_bridge()
-    //    .map(|i| {
-    //        let mut clone = device.clone();
-    //        clone.reg_a = i;
-    //        let res = clone.run();
-    //        (i, res == searched)
-    //    })
-    //    .filter(|(_, success)| *success)
-    //    .collect::<Vec<_>>();
-    //foo.into_iter().min_by(|(a_0, _), (a_1, _)| a_0.cmp(a_1)).unwrap().0
+    foo.unwrap()
 }
 
 #[derive(Clone, Debug)]
@@ -101,20 +64,28 @@ impl Device {
         }
     }
 
-    fn run(&mut self) -> String {
+    fn run(&mut self) -> Vec<usize> {
         let mut output: Vec<usize> = vec![];
 
         while let Some(ins) = self.instructions.get(self.ins_pointer) {
             match ins {
+                // 0
                 Adv(op) => self.reg_a = self.reg_a / 2_usize.pow(self.combo_value(*op) as u32),
+                // 1
                 Bxl(op) => self.reg_b = self.reg_b ^ op,
+                // 2
                 Bst(op) => self.reg_b = self.combo_value(*op) % 8,
+                // 3
                 Jnz(op) => if self.reg_a != 0 {
                     self.ins_pointer = *op / 2
                 }
+                // 4
                 Bxc(_) => self.reg_b = self.reg_b ^ self.reg_c,
+                // 5
                 Out(op) => output.push(self.combo_value(*op) % 8),
+                // 6
                 Bdv(op) => self.reg_b = self.reg_a / 2_usize.pow(self.combo_value(*op) as u32),
+                // 7
                 Cdv(op) => self.reg_c = self.reg_a / 2_usize.pow(self.combo_value(*op) as u32)
             };
 
@@ -127,7 +98,54 @@ impl Device {
             }
         }
 
-        output.into_iter().map(|val| val.to_string()).join(",")
+        output
+    }
+
+    fn run_b(&self) -> Option<usize> {
+        let mut selection = vec![];
+        self.run_b_rec(self.original_instructions.len(), &mut selection)
+    }
+
+    fn run_b_rec(&self, index: usize, selection: &mut Vec<usize>) -> Option<usize> {
+        let get_a = |selection: &Vec<usize>| selection.iter().fold(0, |mut acc, item| {
+            acc <<= 3;
+            acc |= item;
+            acc
+        });
+
+        if index == 0 {
+            return Some(get_a(selection));
+        }
+
+        for i in 0..=7 {
+            selection.push(i);
+
+            let a = get_a(selection);
+
+            let mut clone = self.clone();
+            clone.reg_a = a;
+
+            let result = clone.run();
+            let expected = &self.original_instructions[(index - 1)..];
+
+            if &result == expected {
+                match self.run_b_rec(index - 1, selection) {
+                    Some(a) => return Some(a),
+                    None => {
+                        selection.pop();
+                    }
+                }
+            } else {
+                selection.pop();
+            }
+        }
+
+        let a = get_a(selection);
+
+        let mut clone = self.clone();
+        clone.reg_a = a;
+
+        None
     }
 
     fn combo_value(&self, val: usize) -> usize {
