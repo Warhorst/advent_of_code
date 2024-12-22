e6638af4d5201a7c5c3c121c3ff5285ceee723b3
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-12-22 18:34:47 +0100

    Solved 2024 22 B

--- src/y2024/d22.rs
+++ src/y2024/d22.rs
@@ -10,40 +10,44 @@ pub fn solve_a(input: &str) -> usize {
 }
 
 pub fn solve_b(input: &str) -> usize {
-    let last_place = |num: usize| num % 10;
-
-    // todo This produces a slightly off result, and I currently dont know why. The resulting value
-    //  is like one price higher than my actual value (I got the star by trial and error).
-    //  But the example works...
+    // the price is the first digit of the given number
+    let get_price = |num: usize| num % 10;
 
     input
         .lines()
         .map(|line| parse::<usize>(line))
+        // calculate the next 2000 numbers, prices and the diffs
         .map(|num| (0..2000)
             .into_iter()
-            .fold((num, vec![last_place(num)], vec![0isize]), |(current_num, mut prices, mut diffs), _| {
+            .fold((num, vec![get_price(num)], vec![]), |(current_num, mut prices, mut diffs), _| {
                 let next = calculate_next_number(current_num);
-                prices.push(last_place(next));
+                prices.push(get_price(next));
                 diffs.push(prices[prices.len() - 1] as isize - prices[prices.len() - 2] as isize);
                 (next, prices, diffs)
             })
         )
+        // the number is no longer needed, keep only the prices and diffs
         .map(|(_, prices, diffs)| (prices, diffs))
+        // get all the first occurrences of a pattern in the diffs, alongside their price
         .flat_map(|(prices, diffs)| diffs
             .windows(4)
             .map(|w| [w[0], w[1], w[2], w[3]])
             .enumerate()
             .fold(HashMap::new(), |mut map, (i, pattern)| {
+                // use this map to only use the first occurrence
                 if !map.contains_key(&pattern) {
-                    map.insert(pattern, prices[i + 3]);
+                    // the price index is 3 (the index of the end of the window) + 1 (because the diffs are one element shorter than the prices)
+                    map.insert(pattern, prices[i + 4]);
                 }
                 map
             })
         )
+        // collect each occurred pattern and sum the prices for them
         .fold(HashMap::with_capacity(50000), |mut map, (pattern, price)| {
             map.entry(pattern).and_modify(|val| *val += price).or_insert(price);
             map
         })
+        // return the max price sum
         .values()
         .copied()
         .max()
