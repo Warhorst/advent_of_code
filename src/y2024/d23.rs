9ab9d6c72e29dcfe7d26ba09848a9212dfc729c4
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-12-24 11:39:05 +0100

    Refactored 2024 23

--- src/y2024/d23.rs
+++ src/y2024/d23.rs
@@ -7,19 +7,17 @@ pub fn solve_a(input: &str) -> usize {
         .map(Connection::from)
         .collect::<Vec<_>>();
 
-    let unique_computer_names = connections
+    let name_connections_map = connections
         .iter()
         .flat_map(|conn| [conn.a.clone(), conn.b.clone()])
-        .collect::<HashSet<_>>();
-
-    let name_connections_map = unique_computer_names
-        .into_iter()
         .map(|com_name| {
             let conns = connections.iter().flat_map(|conn| conn.get_other(&com_name)).collect::<HashSet<_>>();
             (com_name, conns)
         })
         .collect::<HashMap<_, _>>();
 
+    // Just compare the currently viewed computer with the 2-permutations of its connections and
+    // check if they form a group of size 3 (they must all be interconnected)
     let groups = name_connections_map
         .iter()
         .flat_map(|(name, conns)| conns
@@ -40,6 +38,7 @@ pub fn solve_a(input: &str) -> usize {
         )
         .collect::<HashSet<_>>();
 
+    // count the groups where at least one computer starts with 't'
     groups
         .iter()
         .filter(|g| g.iter().any(|name| name.starts_with("t")))
@@ -52,45 +51,56 @@ pub fn solve_b(input: &str) -> String {
         .map(Connection::from)
         .collect::<Vec<_>>();
 
-    let unique_computer_names = connections
+    let name_connections_map = connections
         .iter()
         .flat_map(|conn| [conn.a.clone(), conn.b.clone()])
-        .collect::<HashSet<_>>();
-
-    let name_connections_map = unique_computer_names
-        .into_iter()
         .map(|com_name| {
             let conns = connections.iter().flat_map(|conn| conn.get_other(&com_name)).collect::<HashSet<_>>();
             (com_name, conns)
         })
         .collect::<HashMap<_, _>>();
 
-    // num outgoing connections + itself = max amount of interconnected computers
+    // The maximum amount of members in a fully connected group is the amount of connections a computer
+    // has plus itself. In the example, every computer has the same amount of connections, so the
+    // maximum amount of connections is just the amount of connections of the first computer
     let max_group_size = name_connections_map.iter().next().unwrap().1.len() + 1;
 
+    // The idea is to go from the maximum amount of computers in the group to potentially zero and check if
+    // a group of this size can be formed with anyone. If not, try to lower the expected group size by one
+    // and check again.
     let mut current = max_group_size;
-
     loop {
+        // With the currently expected group size, try to find a set of computers of this size
+        // where all are interconnected to each other.
         let found = name_connections_map
             .iter()
             .find_map(|(name, conns)| {
+                // create a temporary extended connection set which contains also the current computer
                 let mut extended = (*conns).clone();
                 extended.insert((*name).clone());
 
-                let potential = conns
+                // Iterate over every computer in the connection, get its connections and count the amount
+                // of elements in the intersection between its connections and the extended connection set.
+                // Keep only the computers where the intersection is current - 1 big.
+                // This works because for example the current expected group size is 4 (like in the example),
+                // the currently viewed computers ('com') connections must have an intersection size of 3, the
+                // computer 'name' and 2 other. Together with com, this is 4. The computer 'name' must find current - 1
+                // computers in its connection which fulfill this condition. 'name' and the other computers than are
+                // the searched group
+                let set = conns
                     .iter()
-                    .filter(|conn| name_connections_map.get(*conn).unwrap().intersection(&extended).count() == current - 1)
+                    .filter(|com| name_connections_map.get(*com).unwrap().intersection(&extended).count() == current - 1)
                     .collect::<HashSet<_>>();
 
-                if potential.len() == current - 1 {
-                    Some((name, potential))
+                if set.len() == current - 1 {
+                    Some((name, set))
                 } else {
                     None
                 }
             })
-            .map(|(name, potential)| {
+            .map(|(name, set)| {
                 let mut vec = vec![name.clone()];
-                potential.iter().for_each(|com| vec.push((*com).clone()));
+                set.iter().for_each(|com| vec.push((*com).clone()));
                 vec.sort();
                 vec.into_iter().join(",")
             });
