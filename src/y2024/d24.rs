48b33fded0a5553d6659423c7a961684ae37b966
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-12-25 20:12:27 +0100

    Played around with 2024 24 B, no solution yet

--- src/y2024/d24.rs
+++ src/y2024/d24.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::fmt::Formatter;
 use regex::Regex;
 use crate::aoc_lib::*;
 use Operator::*;
@@ -9,6 +10,8 @@ pub fn solve_a(input: &str) -> usize {
 }
 
 pub fn solve_b(input: &str) -> usize {
+    // todo I still have no idea, maybe this will help if I find the energy to try this again: https://www.reddit.com/r/adventofcode/comments/1hla5ql/2024_day_24_part_2_a_guide_on_the_idea_behind_the/
+
     let device = Device::from_input(input);
 
     let x = device.get_x();
@@ -19,8 +22,41 @@ pub fn solve_b(input: &str) -> usize {
     println!("Sum: {sum}");
     let current_z = device.calculate_z();
     println!("Z: {current_z}");
-    println!("Sum Binary: {:#045b}", sum);
-    println!("Z   Binary: {:#045b}", current_z);
+    println!("Sum Binary: {:#046b}", sum);
+    println!("Z   Binary: {:#046b}", current_z);
+
+    let all_gates = device
+        .z_targets()
+        .map(|target| (target, device.get_producing_gates(target)))
+        .inspect(|(target, gates)| {
+            let grouped = gates.iter().fold(HashMap::<usize, Vec<Gate>>::new(), |mut map, (index, gate)| {
+                map.entry(*index).and_modify(|index_gates| index_gates.push(gate.clone())).or_insert(vec![gate.clone()]);
+                map
+            });
+
+            println!("{target}");
+            for i in 0..grouped.len() {
+                grouped.get(&i).unwrap().iter().for_each(|gate| print!("{}    ", gate));
+                println!()
+            }
+
+            //println!("{target}: Gates: {:?}, Num Gates: {}", gates, gates.len())
+        })
+        .fold(HashSet::new(), |mut all_gates, (_, gates)| {
+            all_gates.extend(gates);
+            all_gates
+        });
+
+    println!("All gates: {}", all_gates.len());
+
+    //let mut xs = all_gates.iter().flat_map(|(_, gate)| [gate.a.clone(), gate.b.clone()]).filter(|origin| origin.starts_with("x")).collect::<Vec<_>>();
+    //xs.sort();
+    //xs.dedup();
+    //println!("XS: {:?}", xs);
+    //let mut ys = all_gates.iter().flat_map(|(_, gate)| [gate.a.clone(), gate.b.clone()]).filter(|origin| origin.starts_with("y")).collect::<Vec<_>>();
+    //ys.sort();
+    //ys.dedup();
+    //println!("YS: {:?}", ys);
 
     //target_expressions_map
     //    .keys()
@@ -37,36 +73,9 @@ pub fn solve_b(input: &str) -> usize {
     0
 }
 
-#[allow(dead_code)]
-fn get_sub_gates(
-    name: &String,
-    target_expressions_map: &HashMap<String, (String, Operator, String)>
-) -> HashSet<String> {
-    let mut sub_gates = HashSet::new();
-    add_sub_gates(name, target_expressions_map, &mut sub_gates);
-    sub_gates
-}
-
-#[allow(dead_code)]
-fn add_sub_gates(
-    name: &String,
-    target_expressions_map: &HashMap<String, (String, Operator, String)>,
-    sub_gates: &mut HashSet<String>
-) {
-    sub_gates.insert(name.clone());
-
-    match target_expressions_map.get(name) {
-        Some((a, _, b)) => {
-            add_sub_gates(a, target_expressions_map, sub_gates);
-            add_sub_gates(b, target_expressions_map, sub_gates);
-        }
-        None => {}
-    }
-}
-
 struct Device {
     initial_values: HashMap<String, bool>,
-    target_expressions_map: HashMap<String, (String, Operator, String)>
+    target_gate_map: HashMap<String, Gate>,
 }
 
 impl Device {
@@ -85,7 +94,7 @@ impl Device {
 
         let expressions_regex = Regex::new(r"(?m)^(.{3})\s(.*)\s(.{3})\s->\s(.{3})$").unwrap();
 
-        let target_expressions_map = regex_captures(
+        let target_gate_map = regex_captures(
             split.next().unwrap(),
             &expressions_regex,
             |caps| {
@@ -100,13 +109,17 @@ impl Device {
                 };
                 let target = caps[3].to_string();
 
-                (target, (a, op, b))
+                (target, Gate {
+                    a,
+                    op,
+                    b,
+                })
             },
         ).into_iter().collect::<HashMap<_, _>>();
 
         Device {
             initial_values,
-            target_expressions_map
+            target_gate_map,
         }
     }
 
@@ -114,11 +127,11 @@ impl Device {
         match self.initial_values.get(name) {
             Some(value) => *value,
             None => {
-                let (a, op, b) = self.target_expressions_map.get(name).unwrap();
-                let a_value = self.get_value(a);
-                let b_value = self.get_value(b);
+                let gate = self.target_gate_map.get(name).unwrap();
+                let a_value = self.get_value(&gate.a);
+                let b_value = self.get_value(&gate.b);
 
-                match op {
+                match gate.op {
                     And => a_value && b_value,
                     Or => a_value || b_value,
                     Xor => (a_value && !b_value) || (!a_value && b_value)
@@ -130,9 +143,9 @@ impl Device {
     fn get_x(&self) -> usize {
         Self::bits_to_num(
             self.initial_values
-            .iter()
-            .filter(|(name, _)| name.starts_with("x"))
-            .map(|(name, value)| (name, *value))
+                .iter()
+                .filter(|(name, _)| name.starts_with("x"))
+                .map(|(name, value)| (name, *value))
         )
     }
 
@@ -147,10 +160,7 @@ impl Device {
 
     fn calculate_z(&self) -> usize {
         Self::bits_to_num(
-            self.target_expressions_map
-            .keys()
-            .filter(|target| target.starts_with("z"))
-            .map(|target| (target, self.get_value(target)))
+            self.z_targets().map(|target| (target, self.get_value(target)))
         )
     }
 
@@ -168,9 +178,49 @@ impl Device {
             acc
         })
     }
+
+    fn z_targets(&self) -> impl Iterator<Item=&String> {
+        let mut zs = self.target_gate_map.keys().filter(|target| target.starts_with("z")).collect::<Vec<_>>();
+        zs.sort();
+        zs.into_iter()
+    }
+
+    fn get_producing_gates(&self, target: &String) -> Vec<(usize, Gate)> {
+        let mut gates = Vec::new();
+
+        self.add_producing_gates(target, 0, &mut gates);
+
+        gates
+    }
+
+    fn add_producing_gates(&self, target: &String, depth: usize, gates: &mut Vec<(usize, Gate)>) {
+        if let Some(gate) = self.get_gate(target) {
+            gates.push((depth, gate.clone()));
+
+            self.add_producing_gates(&gate.a, depth + 1, gates);
+            self.add_producing_gates(&gate.b, depth + 1, gates);
+        }
+    }
+
+    fn get_gate(&self, target: &String) -> Option<&Gate> {
+        self.target_gate_map.get(target)
+    }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Gate {
+    a: String,
+    b: String,
+    op: Operator,
+}
+
+impl std::fmt::Display for Gate {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {:?} {}", self.a, self.op, self.b)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Operator {
     And,
     Or,
