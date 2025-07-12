4a242a6b4a4b558bb7416baf3a720b9915fdf5b6
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-07-12 19:17:41 +0200

    from_regex now also works for structs

--- proc_macros/src/from_regex.rs
+++ proc_macros/src/from_regex.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use proc_macro2::Ident;
 use quote::{format_ident, quote};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Attribute, Fields, FieldsNamed, FieldsUnnamed, Item, ItemEnum, LitStr, Variant};
+use syn::{parse_macro_input, Attribute, Fields, FieldsNamed, FieldsUnnamed, Item, ItemEnum, ItemStruct, LitStr, Variant};
 // the plan:
 // #[derive(FromRegex)]
 // enum MyVariants {
@@ -35,26 +35,156 @@ use syn::{parse_macro_input, Attribute, Fields, FieldsNamed, FieldsUnnamed, Item
 //      panic!("Could not parse from input '{haystack}'")
 // }
 
-// todo refactoring (for example don't panic, but create compiler errors on failure)
-//  and implement this for structs too
-
 pub(crate) fn create(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as Item);
 
-    let item_enum = match item {
-        Item::Enum(item_enum) => item_enum,
-        _ => return syn::Error::new(
-                item.span(),
-                "Only enums for now"
-            ).to_compile_error().into()
+    match item {
+        Item::Struct(item_struct) => create_for_struct(item_struct),
+        Item::Enum(item_enum) => create_for_enum(item_enum),
+        _ => syn::Error::new(
+            item.span(),
+            "The from_regex proc macro attribute is only allowed on structs and enums!",
+        ).to_compile_error().into()
+    }
+}
+
+fn create_for_struct(item_struct: ItemStruct) -> TokenStream {
+    let static_regex = match create_static_regex_for_struct(&item_struct) {
+        Ok(sr) => sr,
+        Err(err) => return err
     };
 
+    let regex_implementation = create_from_regex_struct_implementation(&item_struct);
+
+    let mut cleaned_struct = item_struct.clone();
+    cleaned_struct.attrs = cleaned_struct.attrs
+        .into_iter()
+        .filter(|attr| !attr.path().is_ident("reg"))
+        .collect();
+
+    quote! {
+        #cleaned_struct
+
+        #static_regex
+
+        #regex_implementation
+    }.into()
+}
+
+fn create_static_regex_for_struct(item_struct: &ItemStruct) -> Result<proc_macro2::TokenStream, TokenStream> {
+    let regex_ident = format_ident!(
+        "{}_REGEX", item_struct.ident.to_string().to_uppercase()
+    );
+    match get_regex_from_attributes(&item_struct.attrs) {
+        Some(regex) => Ok(quote! {static #regex_ident: std::sync::LazyLock<regex::Regex> = std::sync::LazyLock::new(|| regex::Regex::new(#regex).unwrap());}),
+        None => Err(syn::Error::new(
+            item_struct.span(),
+            format!("The struct {} does not have the reg attribute defined!", item_struct.ident),
+        ).to_compile_error().into())
+    }
+}
+
+fn create_from_regex_struct_implementation(item_struct: &ItemStruct) -> proc_macro2::TokenStream {
+    let struct_ident = &item_struct.ident;
+    let generics = &item_struct.generics;
+    let where_clause = &generics.where_clause;
+
+    let regex_ident = format_ident!(
+        "{}_REGEX", item_struct.ident.to_string().to_uppercase()
+    );
+
+    let if_statement = match &item_struct.fields {
+        Fields::Named(f) => create_named_struct_if(struct_ident, &regex_ident, f),
+        Fields::Unnamed(f) => create_unnamed_struct_if(struct_ident, &regex_ident, f),
+        Fields::Unit => create_unit_struct_if(struct_ident, &regex_ident)
+    };
+
+    quote! {
+        impl #generics #struct_ident #generics #where_clause {
+            fn from_regex(haystack: &str) -> Self {
+                #if_statement
+
+                panic!("The regex did not match the haystack '{haystack}!'")
+            }
+        }
+    }
+}
+
+fn create_named_struct_if(
+    struct_ident: &Ident,
+    static_ident: &Ident,
+    f: &FieldsNamed
+) -> proc_macro2::TokenStream {
+    let params = f.named
+        .iter()
+        .enumerate()
+        .map(|(i, f)| {
+            let i = i + 1;
+            let ident = f.ident.as_ref().unwrap();
+            let ty = &f.ty;
+            quote! { #ident: capture
+                .get(#i)
+                .expect(&format!("Expected {} capture in the haystack", #i))
+                .as_str()
+                .parse::<#ty>()
+                .expect(&format!("Failed to parse '{}' as {}", capture.get(#i).unwrap().as_str(), stringify!(#ty)))
+            }
+        });
+
+    quote! {
+        if let Some(capture) = #static_ident.captures(haystack) {
+            return #struct_ident {
+                #(#params),*
+            }
+        }
+    }
+}
+
+fn create_unnamed_struct_if(
+    struct_ident: &Ident,
+    static_ident: &Ident,
+    f: &FieldsUnnamed
+) -> proc_macro2::TokenStream {
+    let params = f.unnamed
+        .iter()
+        .enumerate()
+        .map(|(i, f)| {
+            let i = i + 1;
+            let ty = &f.ty;
+            quote! { capture
+                .get(#i)
+                .expect(&format!("Expected {} capture in the haystack", #i))
+                .as_str()
+                .parse::<#ty>()
+                .expect(&format!("Failed to parse '{}' as {}", capture.get(#i).unwrap().as_str(), stringify!(#ty)))
+            }
+        });
+
+    quote! {
+        if let Some(capture) = #static_ident.captures(haystack) {
+            return #struct_ident(#(#params),*)
+        }
+    }
+}
+
+fn create_unit_struct_if(
+    struct_ident: &Ident,
+    static_ident: &Ident,
+) -> proc_macro2::TokenStream {
+    quote! {
+        if #static_ident.is_match(haystack) {
+            return #struct_ident;
+        }
+    }
+}
+
+fn create_for_enum(item_enum: ItemEnum) -> TokenStream {
     let static_regexes = match create_static_regexes_from_variants(&item_enum) {
         Ok(sr) => sr,
         Err(err) => return err
     };
 
-    let from_regex_implementation = create_from_regex_implementation(&item_enum);
+    let from_regex_implementation = create_from_regex_enum_implementation(&item_enum);
 
     // remove the custom helper attributes from the original input
     let mut cleaned_enum = item_enum.clone();
@@ -106,7 +236,7 @@ fn get_regex_from_attributes<'a>(attrs: impl IntoIterator<Item=&'a Attribute>) -
     regex_attribute.parse_args::<LitStr>().ok()
 }
 
-fn create_from_regex_implementation(item_enum: &ItemEnum) -> proc_macro2::TokenStream {
+fn create_from_regex_enum_implementation(item_enum: &ItemEnum) -> proc_macro2::TokenStream {
     let enum_ident = &item_enum.ident;
     let generics = &item_enum.generics;
     let where_clause = &generics.where_clause;
--- proc_macros/tests/from_regex_tests.rs
+++ proc_macros/tests/from_regex_tests.rs
@@ -3,6 +3,80 @@ use std::str::FromStr;
 use proc_macros::from_regex;
 
 #[test]
+fn struct_all_types() {
+    #[from_regex]
+    #[reg(r#"A (\d+) ([a-zA-Z]+)"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Named {
+        num: usize,
+        string: String
+    }
+
+    #[from_regex]
+    #[reg(r#"B (\d+) ([a-zA-Z]+)"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Unnamed(usize, String);
+
+    #[from_regex]
+    #[reg(r#"C"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Unit;
+
+    assert_eq!(
+        Named {
+            num: 42,
+            string: "Wololo".to_string()
+        },
+        Named::from_regex("A 42 Wololo")
+    );
+    assert_eq!(
+        Unnamed(42, "Wololo".to_string()),
+        Unnamed::from_regex("B 42 Wololo")
+    );
+    assert_eq!(
+        Unit,
+        Unit::from_regex("C")
+    )
+}
+
+#[test]
+fn struct_generics() {
+    #[from_regex]
+    #[reg(r#"A (\d+) ([a-zA-Z]+)"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Named<A, B> where A: FromStr, B: FromStr, <A as FromStr>::Err: Debug, <B as FromStr>::Err: Debug {
+        num: A,
+        string: B
+    }
+
+    #[from_regex]
+    #[reg(r#"B (\d+) ([a-zA-Z]+)"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Unnamed<A, B>(A, B) where A: FromStr, B: FromStr, <A as FromStr>::Err: Debug, <B as FromStr>::Err: Debug;
+
+    #[from_regex]
+    #[reg(r#"C"#)]
+    #[derive(Debug, Eq, PartialEq)]
+    struct Unit;
+
+    assert_eq!(
+        Named {
+            num: 42,
+            string: "Wololo".to_string()
+        },
+        Named::from_regex("A 42 Wololo")
+    );
+    assert_eq!(
+        Unnamed(42, "Wololo".to_string()),
+        Unnamed::from_regex("B 42 Wololo")
+    );
+    assert_eq!(
+        Unit,
+        Unit::from_regex("C")
+    )
+}
+
+#[test]
 fn enum_all_variants() {
     #[from_regex]
     #[derive(Debug, Eq, PartialEq)]
@@ -52,15 +126,15 @@ fn enum_generics() {
     }
 
     assert_eq!(
-        Enum::<usize, String>::A {
+        Enum::A {
             num: 42,
             string: "Wololo".to_string()
         },
-        Enum::<usize, String>::from_regex("A 42 Wololo")
+        Enum::from_regex("A 42 Wololo")
     );
     assert_eq!(
-        Enum::<usize, String>::B(42, "Wololo".to_string()),
-        Enum::<usize, String>::from_regex("B 42 Wololo")
+        Enum::B(42, "Wololo".to_string()),
+        Enum::from_regex("B 42 Wololo")
     );
     assert_eq!(
         Enum::<usize, String>::C,
