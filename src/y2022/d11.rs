dada9e11baba4f25f370af60b741b3365ec2f726
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-11-23 17:13:09 +0100

    Solved 2022 11

--- src/y2022/d11.rs
+++ src/y2022/d11.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Mul};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::{Add, Mul, Rem},
+};
 
 use crate::aoc_lib::parse;
 
@@ -24,26 +27,31 @@ pub fn solve_a(input: &str) -> usize {
     monkeys[0].inspections * monkeys[1].inspections
 }
 
+// https://en.wikipedia.org/wiki/Modular_arithmetic
 pub fn solve_b(input: &str) -> u128 {
-    let mut monkeys = input
+    let monkeys = input
         .split("\n\n")
         .map(|s| Monkey::from(s))
         .collect::<Vec<_>>();
 
+    let divisors = monkeys.iter().map(|m| m.test_div).collect::<HashSet<_>>();
+
+    let mut stress_monkeys = monkeys.into_iter().map(|m| StressMonkey::new(m, divisors.iter())).collect::<Vec<_>>();
+
     for _ in 0..10000 {
-        for i in 0..monkeys.len() {
-            let monkey = monkeys.get_mut(i).unwrap();
-            let changes = monkey.stress_turn();
+        for i in 0..stress_monkeys.len() {
+            let monkey = stress_monkeys.get_mut(i).unwrap();
+            let changes = monkey.turn();
 
             for change in changes {
-                monkeys.get_mut(change.0).unwrap().throw_to(change.1);
+                stress_monkeys.get_mut(change.0).unwrap().throw_to(change.1);
             }
         }
     }
 
-    monkeys.sort_by(|m0, m1| m1.inspections.cmp(&m0.inspections));
+    stress_monkeys.sort_by(|m0, m1| m1.inspections.cmp(&m0.inspections));
 
-    monkeys[0].inspections as u128 * monkeys[1].inspections as u128
+    stress_monkeys[0].inspections as u128 * stress_monkeys[1].inspections as u128
 }
 
 /// Reject humanity. Return to
@@ -70,25 +78,6 @@ impl Monkey {
                     (self.if_true, item)
                 } else {
                     (self.if_false, item)
-                }
-            })
-            .collect()
-    }
-
-    fn stress_turn(&mut self) -> Vec<(usize, u128)> {
-        // todo I might need to keep track of all possible remainders at once
-        
-        self.inspections += self.items.len();
-        self.items
-            .drain(..)
-            .map(|item| Modulo::new(item, self.test_div))
-            .map(|item| self.operation.calc_stress(item)) // inspect
-            .map(|item| {
-                // test
-                if item.remainder == 0 {
-                    (self.if_true, item.remainder)
-                } else {
-                    (self.if_false, item.remainder)
                 }
             })
             .collect()
@@ -156,6 +145,61 @@ impl From<&str> for Monkey {
     }
 }
 
+/// Monkey, but a lot more stressful
+#[derive(Debug)]
+pub struct StressMonkey {
+    items: Vec<SuperModulo>,
+    operation: Operation,
+    test_div: u128,
+    if_true: usize,
+    if_false: usize,
+    inspections: usize,
+}
+
+impl StressMonkey {
+    fn new<'a>(
+        monkey: Monkey,
+        all_divisors: impl Iterator<Item = &'a u128> + 'a,
+    ) -> Self {
+        let all_divisors = all_divisors.copied().collect::<Vec<_>>();
+        StressMonkey {
+            items: monkey
+                .items
+                .into_iter()
+                .map(|item| SuperModulo::new(item, &all_divisors))
+                .collect(),
+            operation: monkey.operation,
+            test_div: monkey.test_div,
+            if_true: monkey.if_true,
+            if_false: monkey.if_false,
+            inspections: monkey.inspections,
+        }
+    }
+
+    fn turn(&mut self) -> Vec<(usize, SuperModulo)> {
+        self.inspections += self.items.len();
+        self.items
+            .drain(..)
+            .map(|item| self.operation.calc_stressful(item)) // inspect
+            .map(|item| {
+                // test
+                if &item % self.test_div == 0 {
+                    (self.if_true, item)
+                } else {
+                    (self.if_false, item)
+                }
+            })
+            .collect()
+    }
+
+    fn throw_to(
+        &mut self,
+        item: SuperModulo,
+    ) {
+        self.items.push(item);
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 enum Operation {
     Add(Value),
@@ -173,13 +217,13 @@ impl Operation {
         }
     }
 
-    fn calc_stress(
+    fn calc_stressful(
         &self,
-        old: Modulo,
-    ) -> Modulo {
+        old: SuperModulo,
+    ) -> SuperModulo {
         match self {
-            Operation::Add(value) => old + Modulo::new(value.get(old.remainder), old.m),
-            Operation::Mul(value) => old * Modulo::new(value.get(old.remainder), old.m),
+            Operation::Add(value) => old.clone() + value.get_stressful(old),
+            Operation::Mul(value) => old.clone() * value.get_stressful(old),
         }
     }
 }
@@ -203,6 +247,20 @@ impl From<&str> for Operation {
     }
 }
 
+impl Rem<u128> for &SuperModulo {
+    type Output = u128;
+
+    fn rem(
+        self,
+        rhs: u128,
+    ) -> Self::Output {
+        match self.mods.get(&rhs) {
+            Some(modulo) => modulo.remainder,
+            None => panic!("Invalid divisor"),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 enum Value {
     Old,
@@ -217,6 +275,73 @@ impl Value {
         match self {
             Value::Old => old,
             Value::Val(v) => *v,
+        }
+    }
+
+    fn get_stressful(
+        &self,
+        old: SuperModulo,
+    ) -> SuperModulo {
+        match self {
+            Value::Old => old,
+            Value::Val(v) => SuperModulo::new(*v, old.get_all_divisors()),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct SuperModulo {
+    mods: HashMap<u128, Modulo>,
+}
+
+impl SuperModulo {
+    fn new<'a>(
+        value: u128,
+        all_divisors: impl IntoIterator<Item = &'a u128> + 'a,
+    ) -> Self {
+        SuperModulo {
+            mods: all_divisors
+                .into_iter()
+                .map(|div| (*div, Modulo::new(value, *div)))
+                .collect(),
+        }
+    }
+
+    fn get_all_divisors(&self) -> impl Iterator<Item = &u128> {
+        self.mods.keys()
+    }
+}
+
+impl Add for SuperModulo {
+    type Output = SuperModulo;
+
+    fn add(
+        self,
+        rhs: Self,
+    ) -> Self::Output {
+        SuperModulo {
+            mods: self
+                .mods
+                .iter()
+                .map(|(div, modulo)| (*div, *modulo + *rhs.mods.get(div).unwrap()))
+                .collect(),
+        }
+    }
+}
+
+impl Mul for SuperModulo {
+    type Output = SuperModulo;
+
+    fn mul(
+        self,
+        rhs: Self,
+    ) -> Self::Output {
+        SuperModulo {
+            mods: self
+                .mods
+                .iter()
+                .map(|(div, modulo)| (*div, *modulo * *rhs.mods.get(div).unwrap()))
+                .collect(),
         }
     }
 }
