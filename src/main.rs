0743c3cdf4b2966434d3048d2bd7227376dac7d0
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-12-14 16:20:26 +0100

    Added a parameter to the aoc binary which allows to execute a selection of examples or puzzles

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 use aoc_solvers::{ExampleResult, RealResult, RunConfig};
 use colored::Colorize;
 
+/// Execute a puzzle for a given year and day.
+///
+/// This binary takes 3 inputs:
+/// 1. The year (required): The year of the puzzle
+/// 2. The day (required): The day of the puzzle
+/// 3. The selection (optional): A string which contains a space separated list of selection identifiers. If not set, every example and puzzle will be executed Possible values:
+///     - ea -> Run example A
+///     - eb -> Run example B
+///     - a -> Run puzzle A
+///     - b -> Run puzzle B
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let year = args[1].parse::<usize>().unwrap();
     let day = args[2].parse::<usize>().unwrap();
-    let run_config = RunConfig {
-        run_example_a: true,
-        run_a: true,
-        run_example_b: true,
-        run_b: true,
+
+    let selection_string = args.get(3);
+
+    let run_config = match selection_string {
+        Some(selection) => {
+            let parts = selection.split(" ").collect::<Vec<_>>();
+
+            RunConfig {
+                run_example_a: parts.contains(&"ea"),
+                run_a: parts.contains(&"a"),
+                run_example_b: parts.contains(&"eb"),
+                run_b: parts.contains(&"b"),
+            }
+        }
+        // No part string is provided, so every example and puzzle will be executed
+        None => RunConfig {
+            run_example_a: true,
+            run_a: true,
+            run_example_b: true,
+            run_b: true,
+        },
     };
 
     let res = match aoc_solvers::solve(day as u8, year as u16, run_config) {
